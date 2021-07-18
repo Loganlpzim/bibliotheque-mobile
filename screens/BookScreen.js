e91e586d8656ac73464716a22578285242234abb
Loganlpzim <lopez.logan.pro6@gmail.com>
2021-07-18 17:48:15 +0200

    add BookScreen and Functionality

--- screens/BookScreen.js
+++ screens/BookScreen.js
@@ -1,30 +1,55 @@
-import React, { Component, useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Alert} from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView, Image} from 'react-native';
 
-export default function BookScreen(route) {
-    const [count, setCount] = useState(0)
-    function callMe() {
-        console.log("Coucou");
-        setCount(count + 1)
-    }
+export default function BookScreen({route}) {
     return (
-      <View style={styles.container}>
-          <Text>Bonjour toi</Text>
-          <TouchableOpacity onPress={callMe}>
-              <View>
-                  <Text>Clique ici {count}</Text>
-              </View>
-          </TouchableOpacity>
-      </View>
-    );
+        <ScrollView>
+            <View style={styles.container}>
+                <Image style={styles.image} source={route.params.img}/>
+            <View style={styles.container}>
+                <Text style={styles.title}>{route.params.titre}</Text>
+                <Text style={styles.description}>{route.params.description}</Text>
+                <Text style={styles.author}>Par: {route.params.author}</Text>
+                <Text style={styles.publishedDate}>Date de parution: {route.params.pb_date}</Text>
+            </View>
+            </View>
+        </ScrollView>
+        )
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
       backgroundColor: 'darkkhaki',
-      alignItems: 'center',
-      justifyContent: 'center',
+    },
+    image: {
+        margin: 'auto',
+        marginTop: 20,
+        marginBottom: 20,
+        width: 250,
+        height: 200
+    },
+    title: {
+        margin: 'auto',
+        fontSize: 18 ,
+        fontWeight: 'bold'
+    },
+    publishedDate: {
+        fontSize: 12,
+        fontWeight: 'bold',
+        margin: 'auto',
+        marginBottom: 20,
+    },
+    description: {
+        marginLeft: 10,
+        marginTop: 10,
+        marginRight: 10
+    },
+    author: {
+        fontSize: 14,
+        fontWeight: 'bold',
+        margin: 'auto',
+        marginTop: 20,
+        marginBottom: 20,
     }
   });
   
--- screens/LibraryScreen.js
+++ screens/LibraryScreen.js
@@ -1,31 +1,90 @@
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Button, TextInput, ScrollView} from 'react-native';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { StyleSheet, Button, TextInput, ScrollView, FlatList, Image} from 'react-native';
+import { ListItem } from 'react-native-elements';
 
 
-export default function LibraryScreen(navigation) {
+export default function LibraryScreen({navigation}) {
+
     const [books , setBooks] = useState([])
     const [value , setValue] = useState("")
     function search(){
         console.log(axios.get(`https://www.googleapis.com/books/v1/volumes?q=${value}&key=AIzaSyCLFnUWgxZWXbpEyudcqfmt3NZd6_a_nBs`)
         .then(response => {
-            setBooks(response.books.data);
+            setBooks(response.data.items);
         }))
     }
+
+    // function goToBook(books) {
+    //     navigation.navigate('Détails du livre');
+    // }
+
+    function goToDetails(item) {
+        navigation.navigate('Détails du livre', {
+                description:item.volumeInfo.description,
+                titre: item.volumeInfo.title,
+                author: item.volumeInfo.authors[0],
+                img: item.volumeInfo.imageLinks.thumbnail,
+                pb_date: item.volumeInfo.publishedDate
+        })
+    }
         return (
-            <ScrollView>
-            <TextInput placeholder="Entrer le titre d'un livre"></TextInput>
-            <Button title="Rechercher" onPress={search}></Button>
+            <ScrollView style={styles.container}>
+            <TextInput placeholder="Entrer le titre d'un livre" 
+            onChangeText={(content) => {setValue(content)}}>
+            </TextInput>
+            <Button style={styles.button} title="Rechercher" onPress={search}></Button>
+                <FlatList
+                    data={books}
+                    renderItem={({item}) => (
+                        <ListItem bottomDivider onPress={() => goToDetails(item)} >
+                        <ListItem.Swipeable
+                                containerStyle= {styles.background}
+                                leftContent={<Button
+                                title = "Détails"
+                                icon = {{ name: 'info', color: 'white' }}
+                                buttonStyle = {{ minHeight: '100%', backgroundColor: '#FE6D73' }}
+                                onPress={() => goToDetails(item)}
+                                />}
+                                rightContent={<Button
+                                title = "Delete"
+                                icon = {{ name: 'delete', color: 'white' }}
+                                buttonStyle = {{ minHeight: '100%', backgroundColor: '#FE6D73' }}
+                                />}>
+                        <ListItem.Content >
+                        <View style={styles.content}>
+                            <Image style={styles.img} source={{uri: "imageLinks" in item.volumeInfo ? item.volumeInfo.imageLinks.thumbnail: 'https://aeroclub-issoire.fr/wp-content/uploads/2020/05/image-not-found-300x225.jpg' }} />
+                            <ListItem.Title style={styles.title}>{item.volumeInfo.title}</ListItem.Title>
+                        </View>
+                        </ListItem.Content>
+                        </ListItem.Swipeable>
+                        </ListItem>)
+                    }
+                    keyExtractor={item => item.id.toString()}/>
             </ScrollView>
         )}
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
       backgroundColor: 'darkkhaki',
-      alignItems: 'center',
-      justifyContent: 'center',
+    },
+    content: {
+      display: 'flex',
+      flexDirection: 'row',
+    },
+    img: {
+        width: 50,
+        height: 100,
+        marginRight: 20
+    },
+    button: {
+        backgroundColor: 'darkkhaki'
+    },
+    title: {
+        fontSize: 16,
+        maxWidth: 250,
     }
   });
-  
+   
   
