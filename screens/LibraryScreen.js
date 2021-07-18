4ca54d6dd4206f13ba065eecf0daf1026ebd986a
Loganlpzim <lopez.logan.pro6@gmail.com>
2021-07-18 14:58:19 +0200

    interact with API

--- screens/LibraryScreen.js
+++ screens/LibraryScreen.js
@@ -1,14 +1,22 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Button, TextInput, View} from 'react-native';
+import { StyleSheet, Button, TextInput, ScrollView} from 'react-native';
 
 
 export default function LibraryScreen(navigation) {
-
+    const [books , setBooks] = useState([])
+    const [value , setValue] = useState("")
+    function search(){
+        console.log(axios.get(`https://www.googleapis.com/books/v1/volumes?q=${value}&key=AIzaSyCLFnUWgxZWXbpEyudcqfmt3NZd6_a_nBs`)
+        .then(response => {
+            setBooks(response.books.data);
+        }))
+    }
         return (
-            <View>
+            <ScrollView>
             <TextInput placeholder="Entrer le titre d'un livre"></TextInput>
-            <Button title="Rechercher"></Button>
-            </View>
+            <Button title="Rechercher" onPress={search}></Button>
+            </ScrollView>
         )}
 
 const styles = StyleSheet.create({
